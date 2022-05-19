59d13b6b7b40a01f29beccc04aafcff3dccea364
wengo500 <wengo.ufo@gmail.com>
2022-05-19 12:24:22 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_authentication_app/bloc/auth/bloc.dart';
+import 'package:flutter_authentication_app/data/repositories/auth_repository.dart';
+import 'package:flutter_authentication_app/firebase_options.dart';
+import 'package:flutter_authentication_app/presentation/dashboard/dashboard.dart';
+import 'package:flutter_authentication_app/presentation/signIn/sign_in.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => AuthRepository(),
+      child: BlocProvider(
+        create: (context) => AuthBloc(
+          authRepository: RepositoryProvider.of<AuthRepository>(context),
+        ),
+        child: MaterialApp(
+          home: StreamBuilder<User?>(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) {
+                // If the snapshot has user data, then they're already signed in. So Navigating to the Dashboard.
+                if (snapshot.hasData) {
+                  return const Dashboard();
+                }
+                // Otherwise, they're not signed in. Show the sign in page.
+                return SignIn();
+              }),
+        ),
+      ),
+    );
+  }
+}
